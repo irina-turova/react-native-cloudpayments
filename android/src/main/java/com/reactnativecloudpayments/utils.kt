d7fba680d41369afe0e36f644d0cd1e3778d7f87
Irina Turova <turova.ia@outlook.com>
2023-10-13 19:13:35 +0200

    feat: upgraded to CloudPayments-SDK-Android:1.3.3; ajusted kotlin mapping

--- android/src/main/java/com/reactnativecloudpayments/utils.kt
+++ android/src/main/java/com/reactnativecloudpayments/utils.kt
@@ -2,27 +2,29 @@ package com.reactnativecloudpayments
 
 import com.facebook.react.bridge.ReadableMap
 import com.fasterxml.jackson.databind.ObjectMapper
+import ru.cloudpayments.sdk.api.models.PaymentDataPayer
 
 // Класс для хранения payment data
 
-data class InitialPaymentData(var paymentData: ReadableMap) {
+data class InitialPaymentData(var paymentData: ReadableMap, val jsonDataString: String?) {
   var publicId: String = paymentData.getString("publicId") as String;
   var accountId: String? = paymentData.getString("accountId");
   var description: String? = paymentData.getString("description");
-  var ipAddress: String?  = paymentData.getString("ipAddress");
+  var ipAddress: String? = paymentData.getString("ipAddress");
   var invoiceId: String? = paymentData.getString("invoiceId");
   var cardHolderName: String? = paymentData.getString("cardHolderName");
   var yandexPayMerchantID: String? = paymentData.getString("yandexPayMerchantID");
   var cultureName: String? = paymentData.getString("cultureName");
-  var payer: String? = paymentData.getString("payer")
-
-  lateinit var jsonDataHash: HashMap<String, Any>
-
-  fun setJsonData(jsonDataString: String) {
-    val objectMapper = ObjectMapper();
-    jsonDataHash = objectMapper.readValue(jsonDataString, Map::class.java) as HashMap<String, Any>;
-  }
+  var payer: PaymentDataPayer? = paymentData.getMap("payer")?.let { loadPaymentDataPayer(it) }
 
   lateinit var totalAmount: String
   lateinit var currency: String
+}
+
+
+fun loadPaymentDataPayer(paymentDataPayer: ReadableMap): PaymentDataPayer {
+  return PaymentDataPayer(
+    firstName = paymentDataPayer.getString("firstName"),
+    lastName = paymentDataPayer.getString("lastName"),
+  )
 }
